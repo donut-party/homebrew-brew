57ddd3c1064d73c7bf2643408437638d982a2b33
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2023-04-14 17:34:21 -0400

    update cli

--- Formula/donut.rb
+++ Formula/donut.rb
@@ -2,10 +2,10 @@ class Donut < Formula
   desc "donut is a single-page app framework for Clojure"
   homepage "https://donut.party/"
   license "MIT"
-  url "https://github.com/donut-party/donut-cli/archive/refs/tags/v0.0.4.zip"
-  sha256 "73a011b4240180de6b216179919e00deedcd97d6337ed1b268aac2af66328848"
+  url "https://github.com/donut-party/donut-cli/archive/refs/tags/v0.0.5.zip"
+  sha256 "07cd420606c75d9f01d10ffebe5456df3f870165d41d6ea4674292055d93c8fd"
 
-  version "0.0.4"
+  version "0.0.5"
 
   depends_on "borkdude/brew/babashka"
 
