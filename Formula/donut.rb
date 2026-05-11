d649598c880e022c7429f3fcbca70ad00e6f06d6
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2026-05-11 15:20:42 -0400

    bump cli

--- Formula/donut.rb
+++ Formula/donut.rb
@@ -1,10 +1,10 @@
 class Donut < Formula
-  v = "0.0.12"
+  v = "0.0.13"
   desc "donut is a single-page app framework for Clojure"
   homepage "https://donut.party/"
   license "MIT"
   url "https://github.com/donut-party/donut-cli/archive/refs/tags/v#{v}.zip"
-  sha256 "4be5a36339b9859ca69c04d6f993b595e9a23bb6e9cea4ef560c4f16446b5a7e"
+  sha256 "aa7309605c8c2e4c265c52b27cf316f9b321bd58022b296aa1e8d563c0bc7b3a"
 
   version v
 
